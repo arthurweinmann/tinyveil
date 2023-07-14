bba754fb53c087473fdb0b154e7f4232c4788d86
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-14 17:19:08 -0500

    fix custom type NullElement

--- tinyveil.js
+++ tinyveil.js
@@ -72,7 +72,7 @@ function AssertTypeOfOR(src, ...t) {
 
 function AssertInstOfOR(src, ...t) {
     for (let i = 0; i < t.length; i++) {
-        if ((src === null && t[i] instanceof NullElement) || src instanceof t[i]) {
+        if ((src === null && typeof t[i] === 'function' && t[i] instanceof NullElement) || src instanceof t[i]) {
             return;
         }
     }
