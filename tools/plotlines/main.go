0128189a188bebe9ff1a256e4057e15e27fcad13
Arthur Weinmann <arthur.weinmann@gmail.com>
2024-01-10 02:00:36 -0600

    plotlines of code tool fixs

--- tools/plotlines/main.go
+++ tools/plotlines/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -14,6 +16,26 @@ import (
 )
 
 func main() {
+	var folder string
+	var fileExt string
+
+	// Define flags with default values
+	flag.StringVar(&folder, "folder", ".", "The folder path to consider")
+	flag.StringVar(&fileExt, "ext", ".js", "The file extension to consider")
+
+	// Parse the command line flags
+	flag.Parse()
+
+	// If the folder flag is not set, use the current working directory
+	if folder == "." {
+		var err error
+		folder, err = os.Getwd()
+		if err != nil {
+			fmt.Println("Error getting current directory:", err)
+			return
+		}
+	}
+
 	cmd := exec.Command("git", "diff-index", "--quiet", "HEAD", "--")
 
 	// Run the git command
@@ -43,7 +65,8 @@ func main() {
 	cmd.Stdout = out
 	err = cmd.Run()
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error running git rev-list command %v - %v\n", err, out.String())
+		return
 	}
 	commits := strings.Split(out.String(), "\n")
 
@@ -55,7 +78,8 @@ func main() {
 	var data []commitData
 
 	// Iterate over the commits.
-	for _, commit := range commits {
+	for i := len(commits) - 1; i > -1; i-- {
+		commit := commits[i]
 		if commit == "" {
 			continue
 		}
@@ -64,27 +88,36 @@ func main() {
 		out, err := checkoutCmd.CombinedOutput()
 		if err != nil {
 			fmt.Printf("Error checking out commit %s: %v - %v\n", commit, err, string(out))
-			continue
+			return
 		}
 
 		// Count the lines of .js files.
-		findCmdString := "find web/ -name '*.js' | xargs wc -l | sort -nr"
+		findCmdString := fmt.Sprintf("find %s -name '*%s' | xargs wc -l | sort -nr", folder, fileExt)
 		findCmd := exec.Command("bash", "-c", findCmdString)
 		var findOut bytes.Buffer
 		findCmd.Stdout = &findOut
 		err = findCmd.Run()
 		if err != nil {
 			fmt.Printf("Error running find command on commit %s: %v\n", commit, err)
-			continue
+			return
 		}
 
-		// Parse the output to get the total line count.
-		linesOutput := strings.Split(findOut.String(), "\n")
-		totalLinesStr := strings.Fields(linesOutput[len(linesOutput)-2])[0]
-		totalLines, err := strconv.Atoi(totalLinesStr)
+		// Split the output into lines
+		lines := strings.Split(findOut.String(), "\n")
+
+		// The total should be in the first line of the output
+		// Trim spaces and split by spaces to isolate the number
+		totalLine := strings.Fields(lines[0])
+		if len(totalLine) < 2 {
+			fmt.Println("Could not parse the total line")
+			return
+		}
+
+		// Convert the total string to an integer
+		totalLines, err := strconv.Atoi(totalLine[0])
 		if err != nil {
-			fmt.Printf("Error parsing line count on commit %s: %v\n", commit, err)
-			continue
+			fmt.Println("Error converting total to integer:", err)
+			return
 		}
 
 		// Store the data.
@@ -93,7 +126,7 @@ func main() {
 
 	// Plot the data.
 	p := plot.New()
-	p.Title.Text = "Lines of JavaScript Code Over Time"
+	p.Title.Text = fmt.Sprintf("Lines of %s Code Over Time in directory %s", fileExt, folder)
 	p.X.Label.Text = "Time"
 	p.Y.Label.Text = "Lines of Code"
 
@@ -109,11 +142,11 @@ func main() {
 	}
 
 	// Save the plot to a PNG file.
-	if err := p.Save(10*vg.Inch, 4*vg.Inch, "loc_over_time.png"); err != nil {
+	if err := p.Save(10*vg.Inch, 4*vg.Inch, "lines_of_code_over_commit_time.png"); err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Plot saved to loc_over_time.png")
+	fmt.Println("Plot saved to lines_of_code_over_commit_time.png")
 
 	// Check out the original branch.
 	output, err := exec.Command("git", "checkout", branchName).CombinedOutput()
